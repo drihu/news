ee694541b39d6a34d28a29f7b2a1fc950aa95ee2
Ricardo Huamani <ricardohuamanip@gmail.com>
2020-05-16 23:34:49 -0500

    refactor

--- app.rb
+++ app.rb
@@ -1,37 +1,25 @@
-require 'http'
 require 'sinatra'
 require 'sinatra/reloader' if development?
 require_relative 'helpers/helper'
 
 helpers Helper
 
-get '/:path?' do |path|
-  @tab = {}
+get /\/(new|ask|show|jobs)?/ do |path|
   page = params.fetch('page', 1).to_i
+  max_pages = get_max_pages(path: path)
+  articles = get_articles(path: path, page: page)
+  erb(:home, locals: {
+    articles: articles,
+    page: page,
+    max_pages: max_pages,
+  })
+end
 
-  case path
-  when nil
-    articles = HTTP.get("https://api.hnpwa.com/v0/news/#{page}.json").parse
-  when 'new'
-    @tab['new'] = 'active'
-    articles = HTTP.get("https://api.hnpwa.com/v0/newest/#{page}.json").parse
-  when 'ask'
-    @tab['ask'] = 'active'
-    articles = HTTP.get("https://api.hnpwa.com/v0/ask/#{page}.json").parse
-  when 'show'
-    @tab['show'] = 'active'
-    articles = HTTP.get("https://api.hnpwa.com/v0/show/#{page}.json").parse
-  when 'jobs'
-    @tab['jobs'] = 'active'
-    articles = HTTP.get("https://api.hnpwa.com/v0/jobs/#{page}.json").parse
-  else
-    halt(404, { 'Content-Type' => 'text/plain' }, 'Unknown.')
-  end
-
-  articles.each_with_index do |article, i|
-    article['host_url'] = "#{article['url'][/(https|http)/]}://#{article['domain']}"
-    article['number'] = 30 * (page - 1) + i + 1
-  end
-
-  erb(:home, locals: { articles: articles, page: page, path: path })
+get '/item' do
+  id = params['id']
+  %{
+    <h1>#{id}</h1>
+    <div>I'm working on this. Please be patient.</div>
+    <a href="/">Go Home</a>
+  }
 end
--- helpers/helper.rb
+++ helpers/helper.rb
@@ -1,4 +1,5 @@
 require 'time_difference'
+require 'http'
 
 module Helper
   # Takes a time object and returns how much time has passed
@@ -7,5 +8,45 @@ module Helper
   # "#{time_since_in_words(test_time)} ago"
   def time_since(date)
     TimeDifference.between(date, Time.now).humanize
+  end
+
+  def get_domain_url(article)
+    scheme = article['url'][/(https|http)/]
+    domain = article['domain']
+    "#{scheme}://#{domain}"
+  end
+
+  def get_articles(path: nil, page: 1)
+    case path
+    when nil
+      articles = HTTP.get("https://api.hnpwa.com/v0/news/#{page}.json").parse
+    when 'new'
+      articles = HTTP.get("https://api.hnpwa.com/v0/newest/#{page}.json").parse
+    when 'ask'
+      articles = HTTP.get("https://api.hnpwa.com/v0/ask/#{page}.json").parse
+    when 'show'
+      articles = HTTP.get("https://api.hnpwa.com/v0/show/#{page}.json").parse
+    when 'jobs'
+      articles = HTTP.get("https://api.hnpwa.com/v0/jobs/#{page}.json").parse
+    else
+      articles = nil
+    end
+    articles.each_with_index do |article, i|
+      article['domain_url'] = get_domain_url(article)
+      article['number'] = 30 * (page - 1) + i + 1
+    end
+    articles
+  end
+
+  def get_max_pages(path: nil)
+    case path
+    when nil then max_pages = 10
+    when 'new' then max_pages = 12
+    when 'ask' then max_pages = 2
+    when 'show' then max_pages = 2
+    when 'jobs' then max_pages = 1
+    else max_pages = nil
+    end
+    max_pages
   end
 end
