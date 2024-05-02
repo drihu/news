3ddf89624bdc97ac13c4c23e9d7c8dbca8b290d2
Ricardo Huamani <ricardohuamanip@gmail.com>
2024-05-02 12:02:56 -0500

    Refactor helper methods

--- app.rb
+++ app.rb
@@ -5,10 +5,10 @@ require_relative 'helpers/helper'
 helpers Helper
 
 get /\/(new|ask|show|jobs)?/ do |path|
+  path = "/#{path}" || '/'
   page = params.fetch('page', 1).to_i
   page_count = get_page_count(path: path)
   articles = get_articles(path: path, page: page)
-  path = "/#{path}" || '/'
 
   erb(:home, locals: {
     articles: articles,
--- helpers/helper.rb
+++ helpers/helper.rb
@@ -4,38 +4,39 @@ module Helper
   API_BASE_URL = 'https://api.hnpwa.com/v0'
 
   PATHS = {
-    nil => {
+    '/' => {
       api_path: '/news',
       api_page_count: 10
     },
-    'new' => {
+    '/new' => {
       api_path: '/newest',
       api_page_count: 10
     },
-    'ask' => {
+    '/ask' => {
       api_path: '/ask',
       api_page_count: 2
     },
-    'show' => {
+    '/show' => {
       api_path: '/show',
       api_page_count: 2
     },
-    'jobs' => {
+    '/jobs' => {
       api_path: '/jobs',
       api_page_count: 1
     }
   }
 
-  def get_articles(path: nil, page: 1)
+  def get_articles(path: '/', page: 1)
     articles = HTTP.get("#{API_BASE_URL}#{PATHS[path][:api_path]}/#{page}.json").parse
 
     articles.each do |article|
       scheme = article['url'][/(https|http)/]
       article['domain_url'] = "#{scheme}://#{article['domain']}"
+      article['title'] = escape_html article['title']
     end
   end
 
-  def get_page_count(path: nil)
+  def get_page_count(path: '/')
     PATHS[path][:api_page_count]
   end
 
