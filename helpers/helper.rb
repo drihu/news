eb554f8dbf985df8374984cdb5c98a2a569281a9
Ricardo Huamani <ricardohuamanip@gmail.com>
2024-05-01 19:37:32 -0500

    Remove time_difference gem because of built-in article's time_ago property

--- helpers/helper.rb
+++ helpers/helper.rb
@@ -1,15 +1,6 @@
-require 'time_difference'
 require 'http'
 
 module Helper
-  # Takes a time object and returns how much time has passed
-  # since that provided time as a humanized string.
-  # test_time = Time.now - (60 * 5) # 5 minutes ago
-  # "#{time_since_in_words(test_time)} ago"
-  def time_since(date)
-    TimeDifference.between(date, Time.now).humanize
-  end
-
   def get_domain_url(article)
     scheme = article['url'][/(https|http)/]
     domain = article['domain']
